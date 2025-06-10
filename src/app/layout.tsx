1ca9e276a9e4ab7abe34523c18c0308a99611860
Francesco Corti <fra@MacBook-Air-di-Francesco.local>
2025-06-10 10:34:52 +0300

    Add LinkedIn Insight Tag for conversion tracking - LinkedIn Partner ID 7349946 with proper Next.js Script component and noscript fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,33 @@ export default function RootLayout({
           `}
         </Script>
         
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight-init" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "7349946";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        
+        {/* LinkedIn Insight Tag - Noscript fallback */}
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=7349946&fmt=gif" />
+        </noscript>
+        
         {/* Remove Next.js development indicators */}
         <Script id="remove-dev-indicators" strategy="afterInteractive">
           {`
