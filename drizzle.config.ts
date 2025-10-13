27c9caa1a6319f40407b0f988427fb14e24c56cd
Karhuno Deploy <karhuno@example.com>
2025-10-13 19:38:44 +0300

    chore: update drizzle configuration for database connection URL
    - Change dbCredentials property from 'connectionString' to 'url'

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,7 @@ import type { Config } from 'drizzle-kit';
 export default {
   out: './drizzle',
   schema: './src/db/schema.ts',
-  driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
